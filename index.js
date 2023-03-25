d20006169da520be6f01644bbc6b955e35c65689
Juan Castañeda castro <juan.castaneda@sofka.local>
2023-03-25 00:32:08 -0500

    Variables de entorno para el despliegue

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 
 // Creamos el servidor
 const app = express();
+app.use(express.static('public'));
+const port = process.env.PORT || 4000;
 
 // Conectamos a la BD
 conectarDB();
@@ -13,6 +15,6 @@ app.use(express.json());
 
 app.use('/api/clientes', require('./routes/clientes'));
 
-app.listen(4000, () => {
+app.listen(port, () => {
     console.log('El servidor esta corriendo perfectamente')
 })
